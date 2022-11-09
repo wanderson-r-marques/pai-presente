3064299026494bf482264a6bb61d68e1484fe1e3
Wanderson R Marques <wrm@cin.ufpe.br>
2022-11-09 17:45:54 -0300

    added theme default

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material"
 import { Routes, Route, Navigate } from "react-router-dom"
 
 export const AppRoutes = () => { 
     return(
         <Routes>
-            <Route path="/inicio" element={<p>Página inicial</p>} />
+            <Route path="/inicio" element={<Button variant="contained" color="primary">Teste</Button>} />
             <Route path="*" element={<Navigate to="/inicio" />} />
         </Routes>
     )
